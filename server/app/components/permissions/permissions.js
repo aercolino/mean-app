d42a75cd89f6ba8911957a1ce335aed265fe4753
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2015-11-27 18:06:30 +0100

    added some notes

--- server/app/components/permissions/permissions.js
+++ server/app/components/permissions/permissions.js
@@ -1,5 +1,12 @@
 {
+    // This is an example of permission using roles for the subject and the object.
+    // Notice that in this case the definition must be an empty object. 
     'Translators canTranslate DocumentsNeedingTranslation': {},
+
+    // This is an example of permission using a role for the subject and an item for the object.
+    // Notice that in this case the definition must specify only the item for the object. 
+    // Notice that the specification entails both a modelName and a restriction.
+    // Notice that the restriction always receives the subject and the object.
     'Translators canTranslate documentsNeedingTranslation': {
         documentsNeedingTranslation: {
             modelName: 'Document',
@@ -8,7 +15,17 @@
             }
         }
     },
-    'anybody canEdit TheirStuff': {},
+
+    // This is an example of permission using an item for the subject and a role for the object.
+    // Notice that in this case the definition must specify only the item for the subject.
+    // Notice that if there is no restriction, then the specification can be simpler.
+    'anybody canEdit TheirStuff': {
+        anybody: 'User'
+    },
+
+    // This is an example of permission using items for the subject and the object.
+    // Notice that in this case the definition must specify both the items for the subject and the object.
+    // Notice that theirStuff is an implementation of Duck Typing.
     'anybody canEdit theirStuff': {
         anybody: 'User',
         theirStuff: {
@@ -18,6 +35,10 @@
             }
         }
     },
+
+    // This is an example of permission using items for the subject and the object.
+    // Notice that using a match-all RegExp for the action and the object makes this permission quite generic.
+    // Notice that (as a policy) a given subject / object (a) matches a string or a regular expression definition (x) iif a.replace(x, '') === ''.
     'anyAdmin canDo everything': {
         anyAdmin: {
             modelName: 'User',
