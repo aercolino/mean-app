a1eb49ac1e5fe058c53049fb9a97b1aa5d410566
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2015-11-27 18:18:50 +0100

    improved notes

--- server/app/components/permissions/permissions.js
+++ server/app/components/permissions/permissions.js
@@ -1,12 +1,26 @@
 {
     // This is an example of permission using roles for the subject and the object.
-    // Notice that in this case the definition must be an empty object. 
+
+    // Notice that 
+    // -- the key is at the same time a unique name and a description of the permission.
+    // -- roles are in UpperCamelCase and items are in lowerCamelCase.
+    
+    // Notice that in this case 
+    // -- the definition must be an empty object. 
+
     'Translators canTranslate DocumentsNeedingTranslation': {},
 
+
+
     // This is an example of permission using a role for the subject and an item for the object.
-    // Notice that in this case the definition must specify only the item for the object. 
-    // Notice that the specification entails both a modelName and a restriction.
-    // Notice that the restriction always receives the subject and the object.
+
+    // Notice that 
+    // -- the specification entails both a modelName and a restriction.
+    // -- the restriction always receives two arguments: the subject and the object.
+
+    // Notice that in this case 
+    // -- the definition must specify only the item for the object. 
+
     'Translators canTranslate documentsNeedingTranslation': {
         documentsNeedingTranslation: {
             modelName: 'Document',
@@ -17,15 +31,25 @@
     },
 
     // This is an example of permission using an item for the subject and a role for the object.
-    // Notice that in this case the definition must specify only the item for the subject.
-    // Notice that if there is no restriction, then the specification can be simpler.
+
+    // Notice that 
+    // -- if there is no restriction, then the specification can be simpler.
+
+    // Notice that in this case 
+    // -- the definition must specify only the item for the subject.
+
     'anybody canEdit TheirStuff': {
         anybody: 'User'
     },
 
+
+
     // This is an example of permission using items for the subject and the object.
-    // Notice that in this case the definition must specify both the items for the subject and the object.
-    // Notice that theirStuff is an implementation of Duck Typing.
+
+    // Notice that in this case 
+    // -- the definition must specify both the items for the subject and the object.
+    // -- theirStuff is an implementation of Duck Typing.
+
     'anybody canEdit theirStuff': {
         anybody: 'User',
         theirStuff: {
@@ -36,9 +60,16 @@
         }
     },
 
+
+
     // This is an example of permission using items for the subject and the object.
-    // Notice that using a match-all RegExp for the action and the object makes this permission quite generic.
-    // Notice that (as a policy) a given subject / object (a) matches a string or a regular expression definition (x) iif a.replace(x, '') === ''.
+
+    // Notice that 
+    // -- as a policy, a given subject/object A matches a string or a regular expression definition X iif A.replace(X, '') === ''.
+
+    // Notice that in this case 
+    // -- a match-all RegExp for the action and the object makes this permission quite generic.
+
     'anyAdmin canDo everything': {
         anyAdmin: {
             modelName: 'User',
