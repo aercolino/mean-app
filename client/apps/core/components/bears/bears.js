21d741bf4b89a97419a241798a4abf3f189b0a0f
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2016-02-26 15:06:32 +0100

    made more standard

--- client/apps/core/components/bears/bears.js
+++ client/apps/core/components/bears/bears.js
@@ -12,87 +12,94 @@
     });
 
     function main(my) {
+        Init(3);
+
+        var self = {};
         
-        var bears = my.$scope.bears = [];
-        var users = [];
-
-        var method = 3;
-        switch (method) {
-            case 1:
-                my.bearsService.List(function (response) {
-                    if (response.error) {
-                        return my.flashService.error(response.error);
-                    }
-                    bears = response.payload;
-                    setScopeBears();
-                });
-                
-                my.usersService.List(function (response) {
-                    if (response.error) {
-                        return my.flashService.error(response.error);
-                    }
-                    users = response.payload;
-                    setScopeBears();
-                });
-            break;
-
-            case 2:
-                var crudService = my.bearsService.$crudService;
-
-                Promise.all([
-                    my.bearsService.List(),
-                    my.usersService.List()
-                ]).then(function (successResponses) {
-                    var response = crudService.SuccessResponse(successResponses[0]);
-                    bears = response.payload;
-                    response = crudService.SuccessResponse(successResponses[1]);
-                    users = response.payload;
-                    setScopeBears();
-                    my.$scope.$apply();
-                }, function (failureResponse) {
-                    var response = crudService.FailureResponse(failureResponse);
-                    my.flashService.error(response.error);
-                    my.$scope.$apply();
-                });
-            break;
-
-            case 3:
-                var promise1 = my.bearsService.List(function (response) {
-                    if (response.error) {
-                        return my.flashService.error(response.error);
-                    }
-                    bears = response.payload;
-                });
-
-                var promise2 = my.usersService.List(function (response) {
-                    if (response.error) {
-                        return my.flashService.error(response.error);
-                    }
-                    users = response.payload;
-                });
-
-                Promise.all([promise1, promise2]).then(function () {
-                    setScopeBears();
-                    my.$scope.$apply();
-                }, function () {});
-            break;
-        }
-
-        return;
+        return self;
 
 
-        function setScopeBears() {
-            if (! (bears.length && users.length)) {
-                return;
+        function Init(method) {
+            var bears = my.$scope.bears = [];
+            var users = [];
+
+            switch (method) {
+                case 1:
+                    my.bearsService.List(function (response) {
+                        if (response.error) {
+                            return my.flashService.error(response.error);
+                        }
+                        bears = response.payload;
+                        SetScopeBears();
+                    });
+                    
+                    my.usersService.List(function (response) {
+                        if (response.error) {
+                            return my.flashService.error(response.error);
+                        }
+                        users = response.payload;
+                        SetScopeBears();
+                    });
+                break;
+
+                case 2:
+                    var crudService = my.bearsService.$crudService;
+
+                    Promise.all([
+                        my.bearsService.List(),
+                        my.usersService.List()
+                    ]).then(function (successResponses) {
+                        var response = crudService.SuccessResponse(successResponses[0]);
+                        bears = response.payload;
+                        response = crudService.SuccessResponse(successResponses[1]);
+                        users = response.payload;
+                        SetScopeBears();
+                        my.$scope.$apply();
+                    }, function (failureResponse) {
+                        var response = crudService.FailureResponse(failureResponse);
+                        my.flashService.error(response.error);
+                        my.$scope.$apply();
+                    });
+                break;
+
+                case 3:
+                    var promise1 = my.bearsService.List(function (response) {
+                        if (response.error) {
+                            return my.flashService.error(response.error);
+                        }
+                        bears = response.payload;
+                    });
+
+                    var promise2 = my.usersService.List(function (response) {
+                        if (response.error) {
+                            return my.flashService.error(response.error);
+                        }
+                        users = response.payload;
+                    });
+
+                    Promise.all([promise1, promise2]).then(function () {
+                        SetScopeBears();
+                        my.$scope.$apply();
+                    }, function () {});
+                break;
             }
-            _.forEach(bears, function (value, key) {
-                var user = _.find(users, {name: value.name});
-                if (! user) {
+
+            return;
+
+
+            function SetScopeBears() {
+                if (! (bears.length && users.length)) {
                     return;
                 }
-                value.roles = user.roles;
-            });
-            my.$scope.bears = bears;
+                _.forEach(bears, function (value, key) {
+                    var user = _.find(users, {name: value.name});
+                    if (! user) {
+                        return;
+                    }
+                    value.roles = user.roles;
+                });
+                my.$scope.bears = bears;
+            }
         }
         
     }
